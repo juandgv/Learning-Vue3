0ca7dde7ed2c8d2b1638fd5ce7e2e47905ae0f75
JD <juandagilva85@gmail.com>
2025-04-12 13:36:04 -0500

    Add TaskList component and remove unused components and routes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
  <Contador />
+ <TaskList />
 </template>
 
 <script setup>
-import Contador from './components/MiContador.vue';
+// import Contador from './components/MiContador.vue';
+import TaskList from './components/TaskList.vue';
+
 </script>
 
 
