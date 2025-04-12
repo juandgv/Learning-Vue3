3584db313cda6586c667aae70020f11b0037c9cd
JD <juandagilva85@gmail.com>
2025-04-12 18:43:19 -0500

    Add Sidebar component with navigation links for Contador and TaskList

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sidebar">
+        <h2>Side Bar</h2>
+        <ul>
+            <li><router-link to="/miContador">Contador</router-link></li>
+            <li><router-link to="/TaskList">Lista De Tareas</router-link></li>
+        </ul>
+        
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+
+.sidebar {
+    width: 200px;
+    background-color: #F0F0F0;
+    padding: 20px;
+}
+</style>
