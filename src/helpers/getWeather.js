4472a6872aafb19ed5890a4b4d57b2830ac58315
JD <juandagilva85@gmail.com>
2025-04-17 17:43:23 -0500

    Update getWeather helper to fetch current temperature instead of hourly data

--- src/helpers/getWeather.js
+++ src/helpers/getWeather.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const API_URL =
-  'https://api.open-meteo.com/v1/forecast?latitude=6.2518&longitude=-75.5636&hourly=temperature_2m&timezone=auto'
+  'https://api.open-meteo.com/v1/forecast?latitude=6.2518&longitude=-75.5636&current=temperature_2m&timezone=auto&forecast_days=1'
 
 export const getTemperature = async () => {
   const answer = await axios.get(API_URL)
-  const newTemperature = answer.data.hourly.temperature_2m[0]
+  const newTemperature = answer.data.current.temperature_2m
   return newTemperature
 }
