c8ea3388493f69ec04759f9fc022f270eb4c223c
JD <juandagilva85@gmail.com>
2025-04-17 17:43:06 -0500

    Refactor AppHeader to display current temperature and bind mensaje prop; update styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="header">
-    <h3>La temperatura actual es: <Weather></Weather></h3>
+    {{ mensajeRecibido }}
+    <Weather></Weather>
   </div>
 </template>
 
 <script setup>
 import Weather from './AppWeather.vue'
+import { defineProps, ref, watch } from 'vue'
+
+const props = defineProps(['mensaje'])
+const mensajeRecibido = ref(props.mensaje)
+
+watch(
+  () => props.mensaje,
+  (newValue) => {
+    mensajeRecibido.value = newValue
+  }
+)
 </script>
 
 <style scoped>
@@ -14,14 +26,10 @@ import Weather from './AppWeather.vue'
   justify-content: center;
   align-items: center;
   padding: 10px 20px;
-  position: fixed;
   top: 0;
   right: 0;
   left: 0;
   background-color: #333;
   color: white;
-}
-h3 {
-  margin: 0;
 }
 </style>
